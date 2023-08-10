cfaed56fae25300c55c31dead747353dcfd20430
Miriam Ertl <miriam_ertl@web.de>
2023-08-10 12:58:22 +0200

    function for toggleBookmark

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // phew… not a lot going on here. Please add some code!
+const nextButton = document.querySelector('[data-js=data-js="first-bookmark"]');
+nextButton.addEventListener("click", () => {
+  console.log("Go to next question");
+});
+
+toggleModeButton.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
